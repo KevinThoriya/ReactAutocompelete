16ee9d254f3d7eb04a778e2a9785165d94cd9de6
kevinthoriya <kevinthoriya0074@gmail.com>
2023-03-20 20:43:20 +0530

    [Add] clear button
    - added focus efffect on autocomplete.

--- src/component/SearchAutoComplete.tsx
+++ src/component/SearchAutoComplete.tsx
@@ -3,21 +3,30 @@ import "./SearchAutoComplete.css";
 import Cross from '../assets/close.svg';
 import Down from '../assets/down.svg';
 import Search from '../assets/Search.svg';
+import { useState } from "react";
 import useTextControl from "./useTextControl";
 
 type Props = {};
 
 const SearchAutoComplete = (props: Props) => {
   const [controlledText, onChangeText, clearText] = useTextControl();
+  const [isSelectionFocused, setIsSelectionFocus] = useState(false);
+
+  const onFocus = () => setIsSelectionFocus(true);
+  const onBlur = () => setIsSelectionFocus(false);
+
 
   return (
-    <div className="selector-container">
+    <div className={`selector-container ${isSelectionFocused ? 'selector-focused' : ''}`}>
       <img src={Search} width={20} className="selector-searchIcon" />
       <input
         type="text"
         name="search"
         value={controlledText}
         onChange={onChangeText}
+        onFocus={onFocus}
+        onBlur={onBlur}
+
       />
       {controlledText && <div role="button" onClick={clearText} className="selector-iconContainer">
         <img src={Cross} width={20} className="selector-closeIcon" />
